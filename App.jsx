f89f278fbeb5337d81e9cc607d79ce11563f8b36
adrikasingh-305 <singhadrika02@gmail.com>
2026-06-29 18:21:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  let name = "adrika";
+  return (
+    <>
+      <h1>Hii Everyone, I am {name}</h1>
+      <p>I am learning Full stack development</p>
+    </>
+  )
+}
+
+export default App
