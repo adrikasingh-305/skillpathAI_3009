03d0bafe2ca8f912105da27e5191ba07e878e274
adrikasingh-305 <singhadrika02@gmail.com>
2026-06-30 18:06:01 +0530

    Delete component/ui directory

--- component/ui/Badge.jsx
+++
@@ -1,8 +0,0 @@
-const Badge = ({ children, variant = 'neutral', icon }) => (
-  <span className={`badge badge--${variant}`}>
-    {icon && <span>{icon}</span>}
-    {children}
-  </span>
-)
-
-export default Badge;
